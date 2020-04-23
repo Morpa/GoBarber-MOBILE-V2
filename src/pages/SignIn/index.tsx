93fadbc97d6c5ed1f27dd9e04a7765e951239c56
Andre Morpa <andremorpa@gmail.com>
2020-04-23 20:41:21 +0100

    feat(app): add input and buttom components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
@@ -9,7 +12,19 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+
       <Title>Faça o seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Clicado');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
