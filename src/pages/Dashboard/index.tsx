2fe3baec402aa912ac34d8695c15ca850bc9cc8e
Andre Morpa <andremorpa@gmail.com>
2020-06-11 13:05:08 +0100

    feat(providers): get providers from api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,33 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 import * as S from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
-  const { user } = useAuth();
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const { user, signOut } = useAuth();
   const { navigate } = useNavigation();
 
   const navigateToProfile = useCallback(() => {
-    navigate('Profile');
-  }, [navigate]);
+    /*   navigate('Profile'); */
+    signOut();
+  }, [signOut]);
+
+  useEffect(() => {
+    api.get('providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
 
   return (
     <S.Container>
@@ -25,6 +41,12 @@ const Dashboard: React.FC = () => {
           <S.UserAvatar source={{ uri: user.avatar_url }} />
         </S.ProfileButton>
       </S.Header>
+
+      <S.ProvidersList
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        renderItem={({ item }) => <S.UserName>{item.name}</S.UserName>}
+      />
     </S.Container>
   );
 };
