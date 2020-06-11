0f504ab5f17190f1779b275ba5f21ce4fa2a9bff
Andre Morpa <andremorpa@gmail.com>
2020-06-11 13:48:57 +0100

    feat(providers): add providers list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
@@ -23,6 +24,13 @@ const Dashboard: React.FC = () => {
     signOut();
   }, [signOut]);
 
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointment', { providerId });
+    },
+    [navigate]
+  );
+
   useEffect(() => {
     api.get('providers').then((response) => {
       setProviders(response.data);
@@ -45,7 +53,30 @@ const Dashboard: React.FC = () => {
       <S.ProvidersList
         data={providers}
         keyExtractor={(provider) => provider.id}
-        renderItem={({ item }) => <S.UserName>{item.name}</S.UserName>}
+        ListHeaderComponent={
+          <S.ProvidersListTitle>Cabeleireiros</S.ProvidersListTitle>
+        }
+        renderItem={({ item: provider }) => (
+          <S.ProviderContainer
+            onPress={() => navigateToCreateAppointment(provider.id)}
+          >
+            <S.ProviderAvatar source={{ uri: provider.avatar_url }} />
+
+            <S.ProviderInfo>
+              <S.ProviderName>{provider.name}</S.ProviderName>
+
+              <S.ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <S.ProviderMetaText>Segunda à sexta</S.ProviderMetaText>
+              </S.ProviderMeta>
+
+              <S.ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <S.ProviderMetaText>8h às 18H</S.ProviderMetaText>
+              </S.ProviderMeta>
+            </S.ProviderInfo>
+          </S.ProviderContainer>
+        )}
       />
     </S.Container>
   );
